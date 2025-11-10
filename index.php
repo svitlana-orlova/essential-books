60cf74ae83611bea71bf7dbb1e82714187efa64e
Svitlana Orlova <160160484+svitlana-orlova@users.noreply.github.com>
2025-11-10 16:57:44 +0200

    making it pretty

--- index.php
+++ index.php
@@ -20,27 +20,22 @@ function extractFile(string $path) : string | false
 
 function getJson(string $folder) : array
 {
-    return json_decode(extractFile("$folder/toc.json"), true);
+    return json_decode(extractFile("$folder/toc.json"), true) ?? [];
 }
 
 function findRecords(string $folder, int $sub = -1) : array
 {
-    $json = getJson($folder);
-    $res = [];
-    foreach ($json as $title) {
-        if ($sub == -1 && $title[1] == -1) {
-            $res[] = $title;
-        } else if ($title[1] == $sub) {
-            $res[] = $title;
+    return array_filter(getJson($folder), function($title) use ($sub) {
+        if (($sub == -1 && $title[1] == -1) || $title[1] == $sub) {
+            return true;
         }
-    }
-    return $res;
+        return false;
+    });
 }
 
 function findIndex(string $folder, string $name) : int
 {
-    $json = getJson($folder);
-    foreach ($json as $index => $title) {
+    foreach (getJson($folder) as $index => $title) {
         if ($title[0] == $name) {
             return ($title[1] == -1 ? $index : $title[1]);
         }
@@ -63,21 +58,24 @@ function findTitle(string $folder, string $name) : int
 function indexToc(string $folder, $name = '') : string
 {
     $json = findRecords($folder, -1);
-    $out  = '<div id="book-toc"><div class="article toc svelte-1ib47n1">';
     $sub = ($name ? findTitle($folder, $name) : -1);
 
-    foreach ($json as $index => $title) {
-        $out .= '<div class="toc-item"><div class="chapters-toc-item">';
-        $out .= '<span class="no svelte-1ib47n1">'. $index .'</span>';
-        if ($sub != -1 && $sub == $index) {
-            $out .= '<b>' . $title[3] . '</b>';
-        } else {
-            $out .= '<a href="/essential/' . $folder . '/' . $title[0] . '">' . $title[3] . '</a>';
-        }
-        $out .= '</div></div>';
-    }
-    $out .= '</div></div>';
-    return $out;
+ob_start(); ?>
+    <div id="book-toc"><div class="article toc svelte-1ib47n1">
+    <?php foreach ($json as $index => $title): ?>
+       <div class="toc-item"><div class="chapters-toc-item">
+       <span class="no svelte-1ib47n1"><?= $index ?></span>
+        <?php if ($sub != -1 && $sub == $index): ?>
+            <b><?= $title[3] ?></b>
+        <?php else: ?>
+            <a href="/essential/<?= $folder . '/' . $title[0] ?>"><?= $title[3] ?></a>
+        <?php endif; ?>
+        </div></div>
+    <?php endforeach; ?>
+    </div></div>
+<?php
+
+    return ob_get_clean();
 }
 
 function pageToc(string $folder, $name) : string
@@ -86,23 +84,41 @@ function pageToc(string $folder, $name) : string
     $records = findRecords($folder, $index);
     $title = findTitle($folder, $name);
 
-    $out = '<div id="page-toc"><div class="article chapter-toc svelte-1t851gm">';
-    $out .= '<div class="mtoc-0 svelte-1t851gm"><b>';
-    $out .= findRecords($folder, -1)[$title][3];
-    $out .= ':</b></div>';
+ob_start(); ?>
+    <div id="page-toc"><div class="article chapter-toc svelte-1t851gm">
+    <div class="mtoc-0 svelte-1t851gm"><b><?= findRecords($folder, -1)[$title][3]; ?>:</b></div>
+    <?php foreach ($records as $title): ?>
+        <div class="mtoc-1 svelte-1t851gm">
+        <span class="no svelte-1ib47n1">*</span>
+        <?php if ($name == $title[0]): ?>
+            <b> <?= $title[3] ?></b>
+        <?php else: ?>
+            <a href="/essential/<?= $folder . '/'. $title[0] ?>"><?= $title[3] ?></a>
+        <?php endif; ?>
+        </div>
+    <?php endforeach; ?>
+    </div></div>
+<?php
 
-    foreach ($records as $title) {
-        $out .= '<div class="mtoc-1 svelte-1t851gm">';
-        $out .= '<span class="no svelte-1ib47n1">*</span>';
-        if ($name == $title[0]) {
-            $out .= '<b>' . $title[3] . '</b>';
-        } else {
-            $out .= '<a href="/essential/'. $folder . '/'. $title[0] .'">' . $title[3] . '</a>';
-        }
-        $out .= '</div>';
+    return ob_get_clean();
+}
+
+function show404() : string
+{
+    header("HTTP/1.1 404 Not Found");
+    echo "404 Not Found";
+    return  "404 Not Found";
+}
+
+function showPage(mixed $html) : void
+{
+    if ($html) {
+        echo extractFile('/assets/header.html');
+        echo $html;
+        echo extractFile('/assets/footer.html');
+    } else {
+        show404();
     }
-    $out .= '</div></div>';
-    return $out;
 }
 
 function main() : void
@@ -110,30 +126,21 @@ function main() : void
     $request = $_SERVER['REQUEST_URI'];
 
     if (preg_match('#^/essential/(\w+)/?(.*)$#', $request, $matches)) {
-        $folder = $matches[1];
-        $name = $matches[2];
+        list (, $folder, $name) = $matches;
 
         if (!$name || $name == 'index.html') {
-            echo extractFile('/assets/header.html');
-            echo indexToc($folder);
-            echo extractFile('/assets/footer.html');
+            showPage(indexToc($folder));
         } else {
-            $page = extractFile($folder . '/' . $name);
-            if (!$page) {
-                header("HTTP/1.1 404 Not Found");
-                echo "404 Not Found";
-            } else {
-                $page = preg_replace('#<div id="page-toc"></div>#', pageToc($folder, $name), $page);
-                echo extractFile('/assets/header.html');
-                echo preg_replace('#<div id="book-toc"></div>#', indexToc($folder, $name), $page);
-                echo extractFile('/assets/footer.html');
-            }
+            $page = extractFile($folder . '/' . $name) ?: '';
+            $page = preg_replace('#<div id="page-toc"></div>#', pageToc($folder, $name), $page);
+            $page = preg_replace('#<div id="book-toc"></div>#', indexToc($folder, $name), $page);
+            showPage($page);
         }
     } else if (str_ends_with($request, '.css')) {
         header('Content-Type: text/css');
-        echo extractFile($request);
+        echo extractFile($request) ?: show404();
     } else {
-        echo extractFile('/assets/index.html');
+        echo extractFile('/assets/index.html') ?: show404();
     }
 }
 
